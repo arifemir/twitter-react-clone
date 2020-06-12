2b34654e17a4746c20b5e729f2ae1fd16adec9de
aearslanturk <arifarslanturk@gmail.com>
2020-06-12 21:35:49 +0300

    photo component edited;

--- components/photo.js
+++ components/photo.js
@@ -2,7 +2,10 @@ import React from 'react'
 import cn from 'classnames'
 import styles from './photo.module.css'
 
-function Photo({ src, alt }) {
+function Photo({
+  src = 'https://pbs.twimg.com/profile_images/1063166968767885314/VNU_rYIa_400x400.jpg',
+  alt
+}) {
   return (
     <div className={cn(styles.photo)}>
       <img className={cn(styles.img)} src={src} alt={alt} />
